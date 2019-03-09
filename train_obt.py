b5a32e5e8f8605791732f063a3fb6ea92951a96b
Ross Wightman <rwightman@gmail.com>
2019-03-09 13:18:37 -0800

    Begin integration of https://github.com/Kaixhin/Rainbow

--- train_obt.py
+++ train_obt.py
@@ -2,19 +2,23 @@ from obstacle_tower_env import ObstacleTowerEnv
 import sys
 import argparse
 
+
 def run_episode(env):
     done = False
-    reward = 0.0
+    episode_reward = 0.0
     
     while not done:
         action = env.action_space.sample()
         obs, reward, done, info = env.step(action)
-    return reward
+        episode_reward += reward
+    return episode_reward
+
 
 def run_evaluation(env):
     while not env.done_grading():
         run_episode(env)
         env.reset()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -22,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('--docker_training', action='store_true')
     parser.set_defaults(docker_training=False)
     args = parser.parse_args()
-
+    int(3)
     env = ObstacleTowerEnv(args.environment_filename, docker_training=args.docker_training)
     if env.is_grading():
         episode_reward = run_evaluation(env)
